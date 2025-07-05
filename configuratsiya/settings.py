2f28d3f226656189e8d0d53f1ac72a1e39b09a09
Faxriddin Baxtiyorov <faxriddinbaxtiyorov144@gmail.com>
2025-07-06 01:56:25 +0500

    first deploy setup

--- configuratsiya/settings.py
+++ configuratsiya/settings.py
@@ -115,12 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+import os
+
 STATIC_URL = "static/"
+STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
-STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]
